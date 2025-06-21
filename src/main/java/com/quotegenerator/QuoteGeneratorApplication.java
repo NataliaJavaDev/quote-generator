2cb19e9db3c55f1b1befae472eadfd82ee6666bf
NataliaJava <105055910+NataliaJava@users.noreply.github.com>
2025-06-21 18:01:11 +0300

    Add logging.

--- src/main/java/com/quotegenerator/QuoteGeneratorApplication.java
+++ src/main/java/com/quotegenerator/QuoteGeneratorApplication.java
@@ -1,12 +1,15 @@
 package com.quotegenerator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class QuoteGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuoteGeneratorApplication.class, args);
+		log.info("Spring Boot Application Started");
 	}
 }
